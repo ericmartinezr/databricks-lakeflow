072da926c271e49682e228fe32c468cc042253e3
ericmartinezr <eric.martinez@live.cl>
2026-03-22 13:09:30 -0300

    wip

--- resources/emol_job.py
+++ resources/emol_job.py
@@ -1,17 +1,34 @@
-from databricks.bundles.jobs import Job, Task, NotebookTask, TaskDependency
+from databricks.bundles.jobs import (
+    Job,
+    Task,
+    NotebookTask,
+    TaskDependency,
+    JobParameterDefinition,
+)
 
 extract_links = Task(
     task_key="extract_links",
     notebook_task=NotebookTask(
         notebook_path="src/tasks/extract_links.py",
-        base_parameters={"run_date": "{{ job.trigger.time.iso_date }}"},
+        base_parameters={"run_date": "{{ job.parameters.run_date }}"},
     ),
 )
 
 extract_data = Task(
     task_key="extract_data",
     depends_on=[TaskDependency(task_key="extract_links")],
-    notebook_task=NotebookTask(notebook_path="src/tasks/extract_data.py"),
+    notebook_task=NotebookTask(
+        notebook_path="src/tasks/extract_data.py",
+        base_parameters={"run_date": "{{ job.parameters.run_date }}"},
+    ),
 )
 
-job = Job(name="emol_job", tasks=[extract_links, extract_data])
+job = Job(
+    name="emol_job",
+    tasks=[extract_links, extract_data],
+    parameters=[
+        JobParameterDefinition(
+            name="run_date", default="{{ job.trigger.time.iso_date }}"
+        ),
+    ],
+)
--- src/tasks/extract_data.py
+++ src/tasks/extract_data.py
@@ -3,8 +3,8 @@
 # TODO: Scrapea e inserta datos a alguna tabla en Databricks
 from databricks.sdk.runtime import dbutils
 
-run_date = dbutils.jobs.taskValues.get(
-    taskKey="extract_links", key="run_date", debugValue=None
-)
+dbutils.widgets.text("run_date", "")
+run_date = dbutils.widgets.get("run_date")
+
 
 print(f"Run date {run_date}")
